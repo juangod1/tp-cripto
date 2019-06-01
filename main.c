d1eb7db8747f33a4b878025b83e78cfc60a12074
Martina Arco <marco@itba.edu.ar>
2019-06-01 13:41:31 -0300

    removed gsl library and added link in cmake

--- main.c
+++ main.c
@@ -4,10 +4,9 @@
 
 #include "main.h"
 #include "commons.h"
-#include <gsl/gsl_matrix.h>
 
-int matrix_example(){
-    gsl_matrix * m = gsl_matrix_alloc(5, 5);
+void main(void){
+    gsl_matrix * m = gsl_matrix_calloc(5, 5);
     double a = gsl_matrix_get(m,2,3);
     print_matrix(m);
     gsl_matrix_free(m);

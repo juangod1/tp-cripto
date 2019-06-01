c5d48381f01b7789e93274d6e11319adf81183ae
Juan Godfrid <juan.godfrid@gmail.com>
2019-06-01 11:06:43 -0300

    added library and random

---
+++ random.c
@@ -0,0 +1,15 @@
+//
+// Created by Juan Godfrid on 2019-06-01.
+//
+
+#include <stdint.h>
+#include "random.h"
+
+void setSeed(int64_t s) {
+    seed = (s ^ 0x5DEECE66DL) & ((1LL << 48) - 1);
+}
+
+uint8_t nextChar(void){
+    seed = (seed * 0x5DEECE66DL + 0xBL) & ((1LL << 48) - 1);
+    return (uint8_t)(seed>>40);
+}
---
+++ random.h
@@ -0,0 +1,11 @@
+//
+// Created by Juan Godfrid on 2019-06-01.
+//
+
+#ifndef TP_CRIPTO_RANDOM_H
+#define TP_CRIPTO_RANDOM_H
+
+void setSeed(int64_t s);
+uint8_t nextChar(void);
+
+#endif //TP_CRIPTO_RANDOM_H

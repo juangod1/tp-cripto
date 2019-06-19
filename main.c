e828f55915911d05558df2bd379e6968a0cc39fe
Juan Godfrid <juan.godfrid@gmail.com>
2019-06-19 20:12:13 -0300

    test option

--- main.c
+++ main.c
@@ -30,22 +30,12 @@ int validate_params(char* secret_image_path, char* watermark_image_path, char* d
 
 // TODO: seguir validando
 int main(int argc, char *argv[]){
-    main_test();
-    char * secret_img_path = "image/secret.bmp";
-    char * watermark_img_path = "image/watermark.bmp";
-    char ** arr = malloc(4*sizeof(char *));
-    arr[0] = "image/shard1.bmp";
-    arr[1] = "image/shard2.bmp";
-    arr[2] = "image/shard3.bmp";
-    arr[3] = "image/shard4.bmp";
-    encrypt_image(secret_img_path, watermark_img_path,arr,2,4);
-    decrypt_image(2,4,arr,watermark_img_path,secret_img_path);
-    free(arr);
-/*    int opt;
+    int opt;
     enum { ENCRYPTION, DECRYPTION } mode = ENCRYPTION;
     struct option longopts[] = {
             {"distribute", no_argument, NULL, 'd'},
             {"recover", no_argument, NULL, 'r'},
+            {"test", no_argument, NULL, 't'},
             {"secret_image", required_argument, NULL, 's'},
             {"watermark_image", required_argument, NULL, 'm'},
             {"k", required_argument, NULL, 'k'},
@@ -58,7 +48,7 @@ int main(int argc, char *argv[]){
     char* directory = "";
     int k = 0, n = 0;
 
-    while ((opt = getopt_long(argc, argv, ":drs:m:k:n:f:", longopts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, ":drts:m:k:n:f:", longopts, NULL)) != -1) {
         switch (opt) {
             case 'd':
                 mode = ENCRYPTION;
@@ -81,16 +71,19 @@ int main(int argc, char *argv[]){
             case 'f':
                 directory = optarg;
                 break;
+            case 't':
+                main_test();
+                break;
             case ':':
                 /* missing option argument */
-/*
+
                 fprintf(stderr, "%s: option '-%c' requires an argument\n",
                         argv[0], optopt);
                 exit(EXIT_FAILURE);
             case '?':
             default:
                 /* invalid option */
-/*
+
                 fprintf(stderr, "%s: option '-%c' is invalid: ignored\n",
                         argv[0], optopt);
                 exit(EXIT_FAILURE);
@@ -105,5 +98,16 @@ int main(int argc, char *argv[]){
     printf("Watermark image: %s\n", watermark_image_path);
     printf("K: %d\n", k);
     printf("N: %d\n", n);
-    printf("Directory: %s\n", directory);*/
+    printf("Directory: %s\n", directory);
+
+    char * secret_img_path = "../image/secret.bmp";
+    char * watermark_img_path = "../image/watermark.bmp";
+    char ** arr = malloc(4*sizeof(char *));
+    arr[0] = "image/shard1.bmp";
+    arr[1] = "image/shard2.bmp";
+    arr[2] = "image/shard3.bmp";
+    arr[3] = "image/shard4.bmp";
+    encrypt_image(secret_img_path, watermark_img_path,arr,2,4);
+    decrypt_image(2,4,arr,watermark_img_path,secret_img_path);
+    free(arr);
 }
